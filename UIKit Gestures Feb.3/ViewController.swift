6d34aadaf5736893aa8114b3483b109f944cb1e6
uwagboe <sm.uwagboe@gmail.com>
2020-02-03 12:44:57 -0500

    Initial Commit

---
+++ UIKit Gestures Feb.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit Gestures Feb.3
+//
+//  Created by Pursuit on 2/3/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
